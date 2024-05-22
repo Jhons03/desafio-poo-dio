c4f7471b8cd3dad12b7c79491f9d152b7c5a87cc
Edson Marco <edin2008@yahoo.com.br>
2024-05-22 17:34:07 -0300

    classes criadas e testadas.

---
+++ src/br/com/dio/desafio/dominio/Dev.java
@@ -0,0 +1,69 @@
+package br.com.dio.desafio.dominio;
+
+import java.util.*;
+
+public class Dev {
+    private String nome;
+    //linkedhashset para deixar os curso em ordem de inscrição
+    public Set<Conteudo> conteudosInscritos = new LinkedHashSet<>();
+    public Set<Conteudo> conteudosConcluidos = new HashSet<>();
+
+    public void inscreverBootcamp(Bootcamp bootcamp){
+        //adicionei todos os conteudos do bootcamp
+        this.conteudosInscritos.addAll(bootcamp.getConteudos());
+        //adicionei o deve no bootcamp
+        bootcamp.getDevs().add(this);
+    }
+    public void progredir(){
+        Optional<Conteudo> conteudo = this.conteudosInscritos.stream().findFirst();
+        if (conteudo.isPresent()){
+            conteudosConcluidos.add(conteudo.get());
+            this.conteudosInscritos.remove(conteudo.get());
+        }else{
+            System.err.println("Voce nao esta matriculado em nenhum conteudo.");
+        }
+    }
+
+    public double calcularTotalXp(){
+        return this.conteudosConcluidos.stream()
+                .mapToDouble(Conteudo::calcularXp)
+                .sum();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Set<Conteudo> getConteudosInscritos() {
+        return conteudosInscritos;
+    }
+
+    public void setConteudosInscritos(Set<Conteudo> conteudosInscritos) {
+        this.conteudosInscritos = conteudosInscritos;
+    }
+
+    public Set<Conteudo> getConteudosConcluidos() {
+        return conteudosConcluidos;
+    }
+
+    public void setConteudosConcluidos(Set<Conteudo> conteudosConcluidos) {
+        this.conteudosConcluidos = conteudosConcluidos;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Dev dev = (Dev) o;
+        return Objects.equals(getNome(), dev.getNome()) && Objects.equals(getConteudosInscritos(), dev.getConteudosInscritos()) && Objects.equals(getConteudosConcluidos(), dev.getConteudosConcluidos());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getNome(), getConteudosInscritos(), getConteudosConcluidos());
+    }
+}
